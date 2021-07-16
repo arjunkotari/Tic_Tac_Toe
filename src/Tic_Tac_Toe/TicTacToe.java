49a3c7a0cb8345c33d0e9f4e5087163463da4c98
arjunkotari <arjun.kotari123@gmail.com>
2021-07-16 10:11:26 +0530

    Added:character array with size of 10

--- src/Tic_Tac_Toe/TicTacToe.java
+++ src/Tic_Tac_Toe/TicTacToe.java
@@ -1,7 +1,28 @@
 package Tic_Tac_Toe;
 
 public class TicTacToe {
+	
+	/*
+	 * Creating CreateBoard method
+	 */
+	public static char[] createBoard()
+	{
+		//creating array with size 10 
+		char[] board=new char[10];
+		for(int i=1;i<board.length;i++) {
+			board[i]=' ';
+		}
+		return board;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game...");
+		/*
+		 * calling createBoard method in main method
+		 */
+		char[] board = createBoard();
+		for(int i:board) {
+			System.out.print(i+" ");
+		}
 	}
 }
